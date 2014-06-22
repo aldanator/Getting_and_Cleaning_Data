2b55e66a15d2e92504077f8397fb4d597df3e0e0
Alda Mizaku <aldanator@users.noreply.github.com>
2014-06-22 18:26:27 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,10 @@
+#Loading Lybrary
 library(plyr)
 
+#Setting working directort
 setwd ("/Users/aldamizaku/Documents/Coursera/Getting_and_Cleaning_Data/UCI_HAR_Dataset")
 
+#Creating tes and train variables
 test <- read.csv("./test/subject_test.txt", header = FALSE)
 train <- read.csv("./train/subject_train.txt", header = FALSE)
 names(test) <- c("subjectid")
@@ -18,6 +21,7 @@ trainSet <-cbind(train,activityTrain)
 labels <- read.fwf("./activity_labels.txt", c(1,20))
 names(labels) <- c("activityid", "activityname")
 
+#combinging the datasets
 test_body_acc_x <- read.table("./test/Inertial Signals/body_acc_x_test.txt")
 testSet <- cbind(testSet, apply(test_body_acc_x, c(1), mean))
 testSet <- cbind(testSet, apply(test_body_acc_x, c(1), sd))
@@ -90,6 +94,7 @@ train_total_acc_z <- read.table("./train/Inertial Signals/total_acc_z_train.txt"
 trainSet <- cbind(trainSet, apply(train_total_acc_z, c(1), mean))
 trainSet <- cbind(trainSet, apply(train_total_acc_z, c(1), sd))
 
+#Loading the correct labels
 names(testSet) <- c("subjectid", "activityid", "mean_bodyaccx", "sd_bodyaccx",
                     "mean_bodyaccy", "sd_bodyaccy","mean_bodyaccz", "sd_bodyaccz", 
                     "mean_bodygyrox", "sd_bodygyrox","mean_bodygyroy", "sd_bodygyroy",
@@ -102,9 +107,11 @@ names(trainSet) <- c("subjectid", "activityid", "mean_bodyaccx", "sd_bodyaccx",
                      "mean_totalaccx", "sd_totalaccx","mean_totalaccy", 
                      "sd_totalaccy","mean_totalaccz", "sd_totalaccz")
 
+#Creating the clean dataset
 dataSet <- rbind(testSet, trainSet)
 cleanDataSet <- merge(dataSet, labels,by.x = "activityid")
 
+#Creating the tidy data set
 ddply(cleanDataSet, .(activityname, subjectid), summarize, 
       avg_mean_body_acc_x = mean(mean_bodyaccx),
       avg_sd_body_acc_x = mean(sd_bodyaccx))
